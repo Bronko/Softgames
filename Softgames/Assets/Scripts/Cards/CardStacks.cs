ce4b950be74dd25d00c31740e879cecb396b2652
Pascal <schroeter.games@gmail.com>
2026-01-15 23:57:58 +0100

    Setup for stacking sprites

--- Softgames/Assets/Scripts/Cards/CardStacks.cs
+++ Softgames/Assets/Scripts/Cards/CardStacks.cs
@@ -1,4 +1,31 @@
-﻿public class CardsStacks : AssignmentScreen
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Screen = Unity.Android.Gradle.Manifest.Screen;
+
+public class CardsStacks : AssignmentScreen
 {
+    public Transform CardPrefab;
+    public Sprite CardSprite;
+    public float DistanceFromCamera = 1000;
     
+    public Transform StackOne;
+    public Transform StackTwo;
+    public float Padding = 200;
+    private List<Transform> Cards = new();
+    
+   
+    
+    void Awake()
+    {
+        for (int i = 0; i < 144; i++)
+        {
+            var card = Instantiate(CardPrefab, StackOne);
+            Cards.Add(card);
+            card.Rotate(Vector3.forward, -1 * i);
+            card.transform.localPosition = new Vector3(0, 2 * i, -1 * i);
+        }
+        CardPrefab.gameObject.SetActive(false);
+    }
+
+   
 }

f2ea5ec2433c829eb4807bce21f97001848d931c
Pascal <schroeter.games@gmail.com>
2026-01-16 12:53:04 +0100

    StackExchange animation and logic

---
+++ Softgames/Assets/Scripts/Cards/CardStackAnimationThingy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using DG.Tweening;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+
+public class CardStackAnimationThingy : AnimationThingy
+{
+    private static readonly string CardTrigger = "Go";  
+    public CardsStacks CardStacks;
+    public float SequenceStepTime;
+    
+    private int index = 0;
+
+    private Transform fromStack;
+    private Transform toStack;
+
+    private float timePassed;
+    private bool isPaused;
+    private int iterationDirection = -1;
+
+    void Start()
+    {
+        CardStacks.messageConfirmed += () => isPaused = false;
+        fromStack = CardStacks.StackOne;
+        toStack = CardStacks.StackTwo;
+        index = CardStacks.Cards.Count - 1;
+    }
+
+    bool IsEndIndex()
+    {
+        return (iterationDirection < 0 && index == 0) 
+               || (iterationDirection > 0) && index == CardStacks.Cards.Count - 1;
+    }
+    async void Update()
+    {
+        if (isPaused)
+            return;
+        
+        timePassed += Time.deltaTime;
+        
+        if (timePassed >= 1)
+        {
+            timePassed = 0;
+            isPaused = true;
+            var isEndIndex = IsEndIndex();
+            SetTrigger(CardTrigger, () =>
+            {
+                isPaused = false; //todo: Message, then = isEndIndex;
+                CardStacks.OnCardMoved(isEndIndex);
+            });
+            if (isEndIndex)
+                SwapTargets();
+            else
+                index += iterationDirection;
+        }
+    }
+
+    private void SwapTargets()
+    {
+        iterationDirection *= -1;
+        (toStack, fromStack) = (fromStack,toStack);
+    }
+
+    protected override void HandleTrigger(string name, Action callback)
+    {
+        if (name == CardTrigger)
+        {
+            var seq = DOTween.Sequence();
+            var card = CardStacks.Cards[index];
+            card.transform.SetParent(toStack, true);
+            var cardBasePos = card.transform.position;
+            var targetLayer = iterationDirection < 0 ? (CardStacks.Cards.Count - 1 - index) : index;
+            var targetPos = new Vector3(0, CardStacks.StackYStep * targetLayer, - targetLayer);
+            var rotateByEulers = new Vector3(0, 0, CardStacks.RotationStep * iterationDirection);
+            
+            seq.Append(card.DOMove(new Vector3(0, cardBasePos.y + 250, cardBasePos.z -50), SequenceStepTime)).SetEase(Ease.InOutSine);
+            seq.Join(card.DOScale(2f, SequenceStepTime)).SetEase(Ease.InOutSine);
+            seq.Append(card.DOLocalMove(targetPos, SequenceStepTime)).SetEase(Ease.InOutSine);
+            seq.Join(card.DOScale(1.0f, SequenceStepTime)).SetEase(Ease.InOutSine);
+            seq.Join(card.DOLocalRotate(rotateByEulers, SequenceStepTime,  RotateMode.LocalAxisAdd)).SetEase(Ease.InOutSine);
+            AddTween(seq, callback);
+        }
+    }
+}
--- Softgames/Assets/Scripts/Cards/CardStacks.cs
+++ Softgames/Assets/Scripts/Cards/CardStacks.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using Screen = Unity.Android.Gradle.Manifest.Screen;
+using UnityEngine.Serialization;
 
 public class CardsStacks : AssignmentScreen
 {
+    public event Action messageConfirmed;
+    
+    public int CardAmount = 144;
     public Transform CardPrefab;
     public Sprite CardSprite;
     public float DistanceFromCamera = 1000;
@@ -11,21 +16,29 @@ public class CardsStacks : AssignmentScreen
     public Transform StackOne;
     public Transform StackTwo;
     public float Padding = 200;
-    private List<Transform> Cards = new();
+    public float RotationStep = -3;
+    public float StackYStep = 2;
     
-   
+    [DoNotSerialize]
+    public List<Transform> Cards = new();
+    
     
     void Awake()
     {
-        for (int i = 0; i < 144; i++)
+        for (int i = 0; i < CardAmount; i++)
         {
             var card = Instantiate(CardPrefab, StackOne);
             Cards.Add(card);
-            card.Rotate(Vector3.forward, -1 * i);
-            card.transform.localPosition = new Vector3(0, 2 * i, -1 * i);
+            card.Rotate(Vector3.forward, RotationStep * i);
+            card.transform.localPosition = new Vector3(0, StackYStep * i, -1 * i);
         }
         CardPrefab.gameObject.SetActive(false);
     }
 
-   
+
+    public void OnCardMoved(bool endReached)
+    {
+        Debug.Log("CardMoved");
+        //Todo: Counters and message
+    }
 }
